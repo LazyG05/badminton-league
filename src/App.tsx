3c9d7c28dbfbc8bd46b4882130755d06a33e15c0
LazyG05 <toger987@gmail.com>
2025-12-03 22:40:05 +0100

    new draw logic3

--- src/App.tsx
+++ src/App.tsx
@@ -71,14 +71,7 @@ const weekday = (dstr: string) =>
     weekday: "long",
   });
 const key = (a: string, b: string) => [a, b].sort().join("::");
-function shuffle<T>(arr: T[]): T[] {
-  const a = [...arr];
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
-}
+
 
 export type Achievement = {
   id: string;
