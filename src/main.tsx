36be22a8daa5a211ab35e51aeacc5d2b032d1ae0
LazyG05 <toger987@gmail.com>
2025-12-10 10:15:16 +0100

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Analytics } from "@vercel/analytics/react"; 
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <Analytics />  {/* <-- ITT KELL LENNI! */}
+  </StrictMode>
+);
 
 import { getAuth, signInAnonymously } from "firebase/auth";
 
